aac7c4d6fc8531c72fd41d4680c0cfb616158931
Shivam-2310 <nshivam2003@gmail.com>
2025-03-27 00:58:09 +0530

    Initial project setup: Add Maven POM with project dependencies

---
+++ library-book-management-system/src/test/java/com/shivam/library_book_management_system/LibraryBookManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivam.library_book_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBookManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
